5a1c352eed4cf51b94edb569e594711487564629
Zeraltz <mcantillo.gdi@gmail.com>
2022-03-12 03:33:41 -0500

    Finish controlFlow section, practice required

--- JS-basics/controlFlow.js
+++ JS-basics/controlFlow.js
@@ -362,3 +362,37 @@ function calculateAverage(averageArray){
     
 
 }
+
+// exercise 10
+console.log('exercise 10')
+
+showStars(10);
+
+function showStars(rows){
+    for (let row = 1; row <= rows; row++ ){
+        let pattern = '';
+        for(let i = 0; i < row; i++)
+            pattern += '*'
+        console.log(pattern);
+    }
+}
+
+// exercise 11
+
+showPrimes(20);
+
+function showPrimes(limit){
+    for(let number = 2; number <= limit; number++)
+        if (isPrime(number)) console.log(number); 
+        
+          
+    
+}
+
+function isPrime(number){
+    for (let factor = 2; factor < number; factor++)
+        if (number % factor === 0)
+            return false;
+
+    return true;
+}
